4abc7112007ba4ce1cfdd33d9f1528d0c8cfd285
Admin <huynhquangminh2003@gmail.com>
2024-12-28 11:37:47 +0700

    Last view

--- lib/views/wishlist_screen/wishlist_screen.dart
+++ lib/views/wishlist_screen/wishlist_screen.dart
@@ -28,7 +28,46 @@ class WishlistScreen extends StatelessWidget {
               return "No order yet!".text.color(darkFontGrey).makeCentered();
             } else {
               var data = snapshot.data!.docs;
-              return Container();
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            leading: Image.network(
+                              "${data[index]['p_imgs'][0]}",
+                              width: 80,
+                              fit: BoxFit.cover,
+                            ),
+                            title: "${data[index]['p_name']}"
+                                .text
+                                .fontFamily(semibold)
+                                .size(16)
+                                .make(),
+                            subtitle: "${data[index]['p_price']}"
+                                .numCurrency
+                                .text
+                                .fontFamily(semibold)
+                                .color(redColor)
+                                .make(),
+                            trailing:
+                                const Icon(Icons.favorite, color: redColor)
+                                    .onTap(() async {
+                              firestore
+                                  .collection(productCollection)
+                                  .doc(data[index].id)
+                                  .set({
+                                'p_wishlist':
+                                    FieldValue.arrayRemove([currentUser!.uid])
+                              }, SetOptions(merge: true));
+                            }),
+                          );
+                        }),
+                  ),
+                ],
+              );
             }
           }),
     );
